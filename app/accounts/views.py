0855df6a6999d388caa74ea53c1b754534b4f802
ekity1002 <dreamguidance1@gmail.com>
2023-03-11 21:27:46 +0900

    init

--- app/accounts/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic.base import TemplateView
-
-# Create your views here.
-class EmailVerificationSentView(TemplateView):
-    template_name = "account/verification_sent.html"
--- app/snippets/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from snippets.models import Snippet
-
-# admin pass @admin0000
-# Register your models here.
-admin.site.register(Snippet)
